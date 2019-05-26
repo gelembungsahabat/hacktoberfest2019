5de4a99d5a1d94055da2b5f00d62257fa1668183
wildan <wildanrgsspeed@gmail.com>
2019-05-26 15:22:06 +0700

    joss

--- track3.py
+++ track3.py
@@ -1,14 +1,14 @@
-from collections import deque
+#from collections import deque
 import numpy as np
 #import argparse
 import cv2
-import serial
-from serial import Serial
+#import serial
+#from serial import Serial
 import imutils
 import time
 
-arduino = serial.Serial('/dev/ttyUSB0', 115200,timeout=0)  
-arduino.flush()					    
+#arduino = serial.Serial('/dev/ttyUSB0', 115200,timeout=0)  
+#arduino.flush()					    
 x_s=0
 y_s=0
 r_s=0
@@ -24,7 +24,7 @@ greenLower = np.array([160, 48, 26 ])
 greenUpper = np.array([170, 255, 255])
 
 
-pts = deque(maxlen=64)
+#pts = deque(maxlen=64)
 fo = cv2.FONT_HERSHEY_DUPLEX
 w = (200,200,0)
 
@@ -35,47 +35,50 @@ while True:
 	img = imutils.resize(img, width=320)
 	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 	mask = cv2.inRange(hsv, greenLower, greenUpper)
-	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+	cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL,
 		cv2.CHAIN_APPROX_SIMPLE)[-2]
 	center = None
 	if len(cnts) > 0:
 		c = max(cnts, key=cv2.contourArea)
 		((x, y),radius) = cv2.minEnclosingCircle(c)
-		M = cv2.moments(c)
+		#M = cv2.moments(c)
 		if radius > 0:
 			cv2.circle(mask, (int(x), int(y)), 5,(255, 0, 255), 2)
 			cv2.putText(mask,"detected",(30,30),fo,1,w,2)
 
 	
 			if x < 100:
-				cv2.putText(mask,"det",(30,100),fo,1,w,2)
-				data = str("1")
-				arduino.write(data.encode())
+				cv2.putText(mask,"kanan",(30,60),fo,1,w,2) #aslinya 'kiri', berhubung kameranya menghadap depan maka di reverse
+				data = 1
+				#arduino.write(data)
 	
 			if x >= 100 and x < 200:
-				cv2.putText(mask,"det",(30,100),fo,1,w,2)
+				cv2.putText(mask,"tengah",(30,60),fo,1,w,2) 
 				data = str("2")
-				arduino.write(data.encode())
+				#arduino.write(data.encode())
 
 	
 			if x >= 200 and x < 320:
-				cv2.putText(mask,"det",(30,100),fo,1,w,2)
+				cv2.putText(mask,"kiri",(30,60),fo,1,w,2) #aslinya 'kanan', berhubung kameranya menghadap depan maka di reverse
 				data = str("3")
-				arduino.write(data.encode())
+				#arduino.write(data.encode())
 
 		else:
 			data = str("0")
-			arduino.write(data.encode())
+			#arduino.write(data.encode())
 			
 	if len(cnts) == 0:
 		ada_color = 0
 		x = 0
 		y = 0
 		
-	x_s = x
-	y_s = y 
+	#x_s = x
+	#y_s = y 
 
-	pts.appendleft(center)
+	fps = cap.get(cv2.CAP_PROP_FPS)
+	afs = str("Fps {0}".format(fps))
+	cv2.putText(img,afs,(10,10),fo,0.5,w)
+	#pts.appendleft(center)
 	cv2.imshow("Frame", mask)
 	cv2.imshow("img", img)
 	#data = str("%d %d\n") % (x_s, y_s)
@@ -89,4 +92,4 @@ while True:
 cap.release()
 cv2.destroyAllWindows()
 
-	
+
